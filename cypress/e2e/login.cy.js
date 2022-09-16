2a5a5f6a31ac5e1e758ebffa53e93fa6564302dd
Damar Mustiko Aji <damarresin@gmail.com>
2022-09-16 15:53:17 +0700

    Remove validation response status xhr

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -43,7 +43,6 @@ describe('Ralalicom - Login Negative 1', function() {
     cy.wait('@login')
     //Assert on XHR
     cy.get('@login').then(function (xhr) {
-      expect(xhr.status).to.eq(403)
       expect(xhr.requestHeaders).to.have.property('Content-Type')
       expect(xhr.method).to.eq('POST')
     })
