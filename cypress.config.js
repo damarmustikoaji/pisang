40c90bd646e8de8b8a8c83b3f2e70a9d50243845
Damar Mustiko Aji <damarresin@gmail.com>
2022-09-29 02:17:53 +0700

    Updated config

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,11 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots',
     video: true,
     videoCompression: 15,
     videosFolder: 'cypress/videos',
+    videoUploadOnPasses: true
   },
 };
